7900396661ef0e218cdac43f714775f557f5b143
srchrisknight <srchrisknight@gmail.com>
2023-09-08 20:30:10 -0700

    update test.py

--- source/test.py
+++ source/test.py
@@ -1,5 +1,4 @@
 import openai
-
 import os
 from dotenv import load_dotenv
 
@@ -9,41 +8,42 @@ load_dotenv()
 # Access your API key like this
 api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = api_key
-print(api_key)
-
 
 SYSTEM_MESSAGE = {
-    'role':'system',
-    'content':'''  
-        You are a translaton bot. Specifically you run translations back and forth between German and English. 
+    'role': 'system',
+    'content': '''
+        You are a translation bot. Specifically, you run translations back and forth between German and English.
 
-        WHen you detect english you translate it to German, when you detect German you translate it to English. 
-        Every Rresponse is formatted as follows,
-    
+        When you detect English, you translate it to German, and when you detect German, you translate it to English.
+        Every response is formatted as follows,
+
         "Language Detected: <Language>
         Translation: <Translation>
     '''
 }
 
-
 def run_translation(content):
     messages = [
         {
-            'role':'user',
-            'content':content
+            'role': 'user',
+            'content': content
         }
     ]
-    
+
     messages.append(SYSTEM_MESSAGE)
-    
+
     response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            max_tokens=512  # limiting the tokens as we only want the question, no greetings or sign-offs
-        )
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=512  # limiting the tokens as we only want the question, no greetings or sign-offs
+    )
 
     return response['choices'][0]['message']
-        
-        
+
 if __name__ == '__main__':
-    print(run_translation('Hello, My name is Chris. Im excited to see your country!'))
+    while True:
+        user_input = input("Enter a sentence to translate (or 'exit' to quit): ")
+        if user_input.lower() == 'exit':
+            break
+        translation = run_translation(user_input)
+        print(translation)
