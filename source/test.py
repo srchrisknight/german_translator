8b50cc14c209a0fc2ef48ff6a8988cc23304da13
srchrisknight <srchrisknight@gmail.com>
2023-09-08 20:08:36 -0700

    update test.py

--- source/test.py
+++ source/test.py
@@ -1,2 +1,37 @@
-print('this app works!!!')
+import openai
 
+SYSTEM_MESSAGE = {
+    'role':'system',
+    'content':'''  
+        You are a translaton bot. Specifically you run translations back and forth between German and English. 
+
+        WHen you detect english you translate it to German, when you detect German you translate it to English. 
+        Every Rresponse is formatted as follows,
+    
+        "Language Detected: <Language>
+        Translation: <Translation>
+    '''
+}
+
+
+def run_translation(content):
+    messages = [
+        {
+            'role':'user',
+            'content':content
+        }
+    ]
+    
+    messages.append(SYSTEM_MESSAGE)
+    
+    response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            max_tokens=512  # limiting the tokens as we only want the question, no greetings or sign-offs
+        )
+
+        return response['choices'][0]['message']
+        
+        
+if __name__ == '__main__':
+    print run_translation('Hello, My name is Chris. Im excited to see your country!')
